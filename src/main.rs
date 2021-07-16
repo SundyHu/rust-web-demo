b13943db3d9444652d4552913986fce1bb33b566
ryan <hs_18312833057@yeah.net>
2021-07-16 17:04:48 +0800

    添加依赖

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn demo() -> impl Responder {
+    let _ = get_ip().await;
+    HttpResponse::Ok().body("Hello,Rust")
+}
+
+async fn get_ip() -> reqwest::Result<()> {
+    let resp = reqwest::Client::new().get("https://httpbin.org/ip")
+        .send().await?.text().await?;
+    println!("{}", resp);
+    Ok(())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(demo)
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
